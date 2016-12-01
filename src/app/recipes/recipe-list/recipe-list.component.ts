9b5ad76b2bf31b343b30477c52e38e20df3876bc
Chris Raiskin <chris.raiskin@standard.com>
2016-12-01 14:48:56 -0800

    ngIf ngSwitch

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,11 +7,26 @@ import { RecipeItemComponent } from './recipe-item.component';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+
+  // recipe = new Recipe('Dummy', 'Dummy', 'http://thumbs2.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg', [])
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'Schnitzel', 
+      'Very tasty', 
+      'http://www.myjewishlearning.com/wp-content/uploads/2007/01/schnitzel.jpg', 
+      []),
+    new Recipe(
+      'Summer Salad', 
+      'Okayish', 
+      'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg',
+      [])     
+  ];
+
+
 
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
-  recipe = new Recipe('Dummy', 'Dummy', 'http://thumbs2.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg')
 
   constructor() { }
 
